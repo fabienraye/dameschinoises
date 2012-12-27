4fadb15ca7c4bfc5eaffc9d35954de6c7c7ac09a
alexandrescieux <scieux.alexandre@gmail.com>
2012-12-27 20:45:28 +0100

    Update sdl_plus.c

--- sdl_plus.c
+++ sdl_plus.c
@@ -1,24 +1,33 @@
-
-#include "sdl_plus.h"
-
-
-#include "SDL/SDL.h"
 #include <stdlib.h>
 #include <stdio.h>
 #include <SDL/SDL_ttf.h>
 #include <SDL/SDL_image.h>
 
-void pause() {
-  int continuer=1;
+#include "SDL/SDL.h"
+#include "sdl_plus.h"
+
+void pause() 
+{
+
+	int continuer;
+	
+	continuer=1;
+	
 	SDL_Event event;
 
-	while(continuer) 
+	while(continuer)
 	{
+		
 		SDL_WaitEvent(&event);
+		
 		switch(event.type)
 		{
+			
 			case SDL_QUIT :
-				continuer = 0;
+			continuer = 0;
+		
 		}
+		
 	}
+	
 } 
