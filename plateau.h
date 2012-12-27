298f13081e4a875f3255027280c7069a20e6f2a5
alexandrescieux <scieux.alexandre@gmail.com>
2012-12-27 20:33:19 +0100

    Update plateau.h

--- plateau.h
+++ plateau.h
@@ -1,17 +1,26 @@
-typedef struct {
-  SDL_Surface * surface;
+typedef struct 
+{
+	
+	SDL_Surface *surface;
 	SDL_Rect position_pion;
+	
 } Pion;
 
-typedef struct {
-	Pion * tab[60];
+typedef struct 
+{
+	
+	Pion *tab[60];
+	
 } Plateau;
 
-typedef struct {
-	Pion * tab[121];
+typedef struct 
+{
+	
+	Pion *tab[121];
+	
 } Trou;
 
-void init_Position(SDL_Surface *ecran, Plateau * plateau);
-void init_Trou (SDL_Surface * ecran, Trou * trou);
-void init_Pion(Plateau * plateau); 
-void free_plateau(Plateau * plateau) ;
+void init_Position(SDL_Surface *ecran, Plateau *plateau);
+void init_Trou (SDL_Surface *ecran, Trou *trou);
+void init_Pion(Plateau *plateau); 
+void free_plateau(Plateau *plateau) ;
