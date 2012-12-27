fcb557c1397d2f2cc096f6157dee8d803f8507b4
alexandrescieux <scieux.alexandre@gmail.com>
2012-12-27 09:44:34 -0800

    Create plateau.h

---
+++ plateau.h
@@ -0,0 +1,17 @@
+typedef struct {
+  SDL_Surface * surface;
+	SDL_Rect position_pion;
+} Pion;
+
+typedef struct {
+	Pion * tab[60];
+} Plateau;
+
+typedef struct {
+	Pion * tab[121];
+} Trou;
+
+void init_Position(SDL_Surface *ecran, Plateau * plateau);
+void init_Trou (SDL_Surface * ecran, Trou * trou);
+void init_Pion(Plateau * plateau); 
+void free_plateau(Plateau * plateau) ;
