652a63132c8ff5d97d11d4a3addeebb1ae137a44
alexandrescieux <scieux.alexandre@gmail.com>
2012-12-27 09:47:01 -0800

    Create dameschinoises.c

---
+++ dameschinoises.c
@@ -0,0 +1,82 @@
+#include "dameschinoises.h"
+#include <stdio.h>
+#include <string.h>
+
+//////////////////////////////////////////////////////////
+// Dans la librairie
+//
+
+void InitLibrary(char name[50])
+{
+  printf("InitLibrary\n");
+	strcpy(name,"Fabien Picarougne");
+}
+
+void StartMatch(const unsigned int nbJoueur, const EPlayer couleur)
+{
+	printf("StartMatch\n");
+}
+
+void StartGame()
+{
+	printf("StartGame\n");
+}
+
+void EndGame()
+{
+	printf("EndGame\n");
+}
+
+void EndMatch()
+{
+	printf("EndMatch\n");
+}
+
+int NextMove(const SGameState * const gameState, int firstMove, SMove *move)
+{
+	printf("NextMove\n");
+	return(0);
+}
+
+//////////////////////////////////////////////////////////
+// Dans l'exécutable
+//
+
+void deroulement_du_jeu()	
+//int main()
+{
+	//*****// à chaque utilisation de gameState, ne pas oublier de faire une copie de tous les éléments (pas fait ici)
+	SGameState gameState;
+
+	//*****// à faire pour chaque joueur
+	
+	// Chargement de la librairie (chargement des pointeurs de fonctions des fonctions décrites dans "backgammon.h")
+	// -> à ermplacer par votre code de chargement
+	pfInitLibrary j1InitLibrary=InitLibrary;
+	pfStartMatch j1StartMatch=StartMatch;
+	pfStartGame j1StartGame=StartGame;
+	pfEndGame j1EndGame=EndGame;
+	pfEndMatch j1EndMatch=EndMatch;
+	pfNextMove j1NextMove=NextMove;
+	// ...
+	pfNextMove j2NextMove=NextMove;
+
+	// Initialisation de la librairie
+	SMove move;
+	char name[50];
+	j1InitLibrary(name);
+	j1StartMatch(3,color1);
+
+		//*****// à faire pour chaque jeu
+		j1StartGame();
+			//*****// pour chaque joueur, tant que ce n'est pas fini
+			move.startPos=0;
+			move.endPos=0;
+			if (j1NextMove(&gameState,1,move);
+				// ...
+		j1EndGame();
+
+	j1EndMatch();
+
+	return(0);
+}
