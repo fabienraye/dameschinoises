f72df0daf3a4c5fda5992d84cb8b5a018c18f630
Alexandre Scieux <scieux.alexandre@gmail.com>
2012-12-31 17:59:50 +0100

    Update plateau.h

--- plateau.h
+++ plateau.h
@@ -33,5 +33,6 @@ void init_position_pions (SDL_Surface *ecran, Pions *pions);
 void init_position_trous (SDL_Surface *ecran, Plateau *plateau);
 void allocation_memoire_position_pions (Pions *pions);
 void liberation_memoire_position_pions (Pions *pions);
+void afficher_mouvement(int depart, int arrivee, SDL_Surface *ecran, Pions *pions);
 
-void mouvement(int depart, int arrivee, SDL_Surface *ecran, Pions *pions);
+void identifier_pion(int x_souris, int y_souris);
