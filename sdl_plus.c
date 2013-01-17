b579459ec865f22652b95eefab31e484eebb6014
Alexandre Scieux <scieux.alexandre@gmail.com>
2013-01-17 17:35:52 +0100

    Update sdl_plus.c

--- sdl_plus.c
+++ sdl_plus.c
@@ -5,15 +5,23 @@
 #include <SDL/SDL_ttf.h>
 #include <SDL/SDL_image.h>
 
-#include "sdl_plus.h"
 #include "plateau.h"
+#include "sdl_plus.h"
 
-void pause() 
+void pause(SDL_Surface *ecran, Plateau *plateau) 
 {
 
 	int continuer;
 	
+	int id_pion;
+	int id_trou_depart;
+	int id_trou_arrivee;
+	
 	continuer=1;
+	
+	id_pion=-1;
+	id_trou_depart=-1;
+	id_trou_arrivee=-1;
 	
 	SDL_Event evenement;
 
@@ -26,11 +34,55 @@ void pause()
 		{
 			
 			case SDL_QUIT :
-			continuer = 0;
+				
+				continuer = 0;
+			
 			break;
 			
-			case SDL_MOUSEBUTTONUP :
-			identifier_pion(evenement.button.x, evenement.button.y);
+			case SDL_MOUSEBUTTONDOWN :
+				
+				afficher_mouvement(0, 17, ecran, plateau);
+			
+				SDL_Flip(ecran); 
+				
+			/*
+			
+				// Premier clic, attendu sur un pion
+				if (identifier_pion(evenement.button.x, evenement.button.y) != -1 && id_trou_depart == -1)
+				{
+					
+					id_pion=identifier_pion(evenement.button.x, evenement.button.y);
+					id_trou_depart=identifier_trou(evenement.button.x, evenement.button.y);
+					
+				}
+				
+				// Deuxième clic, attendu sur un trou où déplacer le pion
+				else if (identifier_trou(evenement.button.x, evenement.button.y) != -1 && id_trou_depart != -1)
+				{
+					
+					id_trou_arrivee=identifier_trou(evenement.button.x, evenement.button.y);
+					
+					// mouvement(id_trou_depart, id_trou_arrivee, plateau)
+					
+					afficher_mouvement(id_trou_depart, id_trou_arrivee, ecran, plateau);
+					
+					SDL_Flip(ecran); 
+					
+					id_pion=-1;
+					id_trou_depart=-1;
+					id_trou_arrivee=-1;
+					
+				}
+				
+				else
+				{
+					
+					
+					
+				}
+				
+				*/
+			
 			break;
 		
 			default :
@@ -40,4 +92,4 @@ void pause()
 		
 	}
 	
-} 
+}
